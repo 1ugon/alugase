dd4c0332b47a728502b189e4a820db4b5acefe75
mott4 <pedromotta20@gmail.com>
2020-05-23 18:59:57 -0300

    primeiro update
    criação das rotas com react-navigaton e da página home

---
+++ mobile/src/pages/Home/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, FlatList, Text, Image, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+import logoImg from '../../assets/icon.png'
+
+import styles from './styles'
+
+export default function Home() {
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>
+            <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    <Text style={styles.headerTextBold}>3 bikes</Text> prontas para alugar!
+                </Text>
+            </View>
+
+        <Text style={styles.title}>Bem-vindo!</Text>
+        <Text style={styles.description}>Escolha uma bicicleta para alugar.
+        </Text>
+
+        <FlatList 
+        data={[1, 2, 3]}
+        style={styles.list}
+        keyExtractor={bike => String(bike)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+            <View style={styles.bike}>
+                <Text style={styles.bikeProperty}>DONO:</Text>
+                <Text style={styles.bikeValue}>Edson</Text>
+
+                <Text style={styles.bikeProperty}>LOCAL:</Text>
+                <Text style={styles.bikeValue}>Taguatinga</Text>
+
+                <Text style={styles.bikeProperty}>VALOR:</Text>
+                <Text style={styles.bikeValue}>R$100/mês</Text>
+
+                <TouchableOpacity style={styles.detailsButton}
+                onPress={() => {}}>
+                <Text style={styles.detailsButtonText}>Tenho interesse!</Text>
+                <Feather name="heart" size={16} color="#DED118" />
+                </TouchableOpacity>
+            </View>
+        )}
+        />
+
+        </View>
+
+    );
+}
